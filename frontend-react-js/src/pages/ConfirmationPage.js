558c86dd74acf610a02b71aa96e1f0b27721d9be
Ibrahim-saliu <ibrahimsaliu297@outlook.com>
2023-03-18 23:36:47 +0000

    implement recovery page

--- frontend-react-js/src/pages/ConfirmationPage.js
+++ frontend-react-js/src/pages/ConfirmationPage.js
@@ -42,12 +42,12 @@ export default function ConfirmationPage() {
 
   const onsubmit = async (event) => {
     event.preventDefault();
-    setCognitoErrors('')
+    setErrors('')
     try {
       await Auth.confirmSignUp(email, code);
       window.location.href = "/"
     } catch (error) {
-      setCognitoErrors(error.message)
+      setErrors(error.message)
     }
     return false
   }
