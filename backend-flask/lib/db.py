6dff52f5a3381adb6426b3be866696846cc6b044
Ibrahim-saliu <ibrahimsaliu297@outlook.com>
2023-03-25 19:33:24 +0000

    working with psql

---
+++ backend-flask/lib/db.py
@@ -0,0 +1,20 @@
+from psycopg_pool import ConnectionPool
+import os
+
+connection_url = os.getenv("CONNECTION_URL")
+pool = ConnectionPool(connection_url)
+
+def query_wrap_object(template):
+  sql = '''
+  (SELECT COALESCE(row_to_json(object_row),'{}'::json) FROM (
+  {template}
+  ) object_row);
+  '''
+
+def query_wrap_array(template):
+  sql = '''
+  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
+  {template}
+  ) array_row);
+  '''
+
